060a079fe5fddcb5adf1e717feca8b50c624e4cb
Samuel <samyamaya4@gmail.com>
2022-11-19 19:10:32 -0600

    AGREGANDO REGISTRO

--- js/index.js
+++ js/index.js
@@ -3,10 +3,14 @@ console.log("HOLA MUNDO!");
 // let main = document.querySelector("main")
 const conten = document.querySelector("main")
 const products = document.querySelector("#productos")
+const categorias = document.querySelector("#categorias")
+const modalCategorias = document.querySelector(".modal-categorias")
+const categoriasApi = document.querySelector("#categorias-api")
 let dans;
 
 //PRIMERO CARGAMOS LOS ELEMENTOS
 document.addEventListener("DOMContentLoaded",() =>{
+    // CARGANDO LOS DATOS DE LA API
     fetch('https://fakestoreapi.com/products')
     .then(res=>res.json())
     .then(json=>{
@@ -32,6 +36,32 @@ document.addEventListener("DOMContentLoaded",() =>{
             cart.appendChild(button)
         });
     })
+    
+    //MOSTRANDO LAS CATEGORIAS
+    fetch('https://fakestoreapi.com/products/categories')
+            .then(res=>res.json())
+            .then(json=>{
+                const categories = json;
+                console.log(categories)
+                categories.forEach(element =>{
+                    const li = document.createElement("li")
+                    const a = document.createElement("a")
+                    a.textContent = element
+                    categoriasApi.appendChild(li);
+                    li.appendChild(a)
+                })
+            })
+    let contador = 0;
+    categorias.addEventListener("click", e =>{
+        if(contador == 0){
+            modalCategorias.classList = "modal-categorias hability";
+            contador = 1;
+        } else if (contador == 1){
+            modalCategorias.classList = "modal-categorias deshability";
+            contador = 0;
+        } 
+    })
+    
 })
 
 
---
+++ js/registro.js
@@ -0,0 +1,11 @@
+const objectDatos = document.querySelectorAll('form input[type="text"]')
+const inputEnviar = document.querySelector("#enviar")
+let dato = {}
+let contador = 0;
+inputEnviar.addEventListener("click", ()=>{
+    
+})
+
+
+
+
